6e6108b1aee5b27bb5af70d0d813203d08636f4a
Kamalesh S <73590020+kamalss11@users.noreply.github.com>
2021-06-05 22:52:02 +0530

    Update js.js

--- JS/js.js
+++ JS/js.js
@@ -108,11 +108,3 @@ function verification(){
     })
 }
 
-// Signout
-
-function logout(){
-    firebase.auth().signOut()
-    let u = firebase.auth().currentUser
-    console.log(u,"logged out")
-    location.replace("https://phoneauth-dojo.netlify.app/")
-}
