f6656e234c7d564f9c8287753febe34a212d77f0
Kamalesh S <73590020+kamalss11@users.noreply.github.com>
2021-06-05 21:29:03 +0530

    Update js.js

--- JS/js.js
+++ JS/js.js
@@ -66,7 +66,7 @@ function render(){
     window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(recaptcha)
     recaptchaVerifier.render()
     var usr = firebase.auth().currentUser
-     if (usr!=null) {
+     if (usr) {
         console.log("Already logged",usr)
         // let usr = firebase.auth().currentUser
         console.log(usr.phoneNumber)
