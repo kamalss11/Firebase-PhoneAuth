ff9062407a2a27dbf4d60c06fda3b5746c466eba
Kamalesh S <73590020+kamalss11@users.noreply.github.com>
2021-06-06 13:00:03 +0530

    Add files via upload

--- JS/js2.js
+++ JS/js2.js
@@ -1,3 +1,30 @@
+var wel = document.querySelector("#usr"),
+logout = document.querySelector(".outbtn")
+
+window.onload = () =>{
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (!user) {
+            location.replace("https://phoneauth-dojo.netlify.app/")
+        } 
+        else{
+            console.log(user.phoneNumber)
+            wel.innerHTML = user.phoneNumber
+        }
+    });
+    loader()
+}
+
+function loader(){
+    setTimeout(showPage, 3000);
+}
+
+function showPage(){
+    document.getElementById("loads").style.display = "none"
+}
+
+wel.addEventListener("click",function(){
+    logout.classList.toggle("active")
+})
 // Signout
 
 function logout(){
