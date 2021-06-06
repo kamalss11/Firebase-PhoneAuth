8142175d651a8d94538022a7e6e48bd54d1f4009
Kamalesh S <73590020+kamalss11@users.noreply.github.com>
2021-06-06 22:59:21 +0530

    Add files via upload

--- JS/js2.js
+++ JS/js2.js
@@ -63,8 +63,6 @@ var rate = document.querySelector("#rate")
 var service = document.querySelector("#service")
 var staus = document.querySelector("#staus")
 var add = document.querySelector("#add")
-const db = firebase.firestore
-console.log(db)
 sbtn.addEventListener("click",function(){
     let nameInput = nam.value
     let phoneInput = phone.value
