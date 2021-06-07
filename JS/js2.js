469284ac44bc0195e3db51b9090341fb05ae64da
Kamalesh S <73590020+kamalss11@users.noreply.github.com>
2021-06-07 08:59:44 +0530

    Add files via upload

--- JS/js2.js
+++ JS/js2.js
@@ -79,7 +79,7 @@ sbtn.addEventListener("click",function(e){
     let statusInput = sts.value
     let addInput = add.value
     console.log(nameInput,phoneInput,priceInput,rateInput,serviceInput,statusInput,addInput)
-    db.doc().add({
+    db.add({
         Name: nameInput,
         Phone: phoneInput,
         Price: priceInput,
