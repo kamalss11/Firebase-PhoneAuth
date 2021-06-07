a578192e3fe4ad42ec8a7992f0a1ee04773207e1
Kamalesh S <73590020+kamalss11@users.noreply.github.com>
2021-06-08 01:37:15 +0530

    Add files via upload

--- JS/js2.js
+++ JS/js2.js
@@ -80,7 +80,7 @@ for(let i=0;i<fields1.length;i++){
 
 function blur(e,num){
     let ph = /\d[0-9]{9,}$/
-    let rt = /\d[0-9]{1,}$/
+    let rt = /\d[0-9]{0,}$/
 
     if(inputs[num].value == ''){
         errors[num].classList.add("active")
