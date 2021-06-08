0896d8fd85bd9d79385426c2e675499c64de1c06
Kamalesh S <73590020+kamalss11@users.noreply.github.com>
2021-06-08 14:49:51 +0530

    Update js2.js

--- JS/js2.js
+++ JS/js2.js
@@ -324,9 +324,9 @@ sbtn2.addEventListener("click",function(e){
     let date = new Date()
     let year = date.getFullYear()
     let doclen
-    let documents = db.get().then function(snapshot) =>{
+    let documents = db.get().then function((snapshot) =>{
        doclen = snapshot.docs().length
-    }
+    })
     let ndoc = 100 + (doclen + 1)
     let id = ("ON"+year+ndoc)
 
