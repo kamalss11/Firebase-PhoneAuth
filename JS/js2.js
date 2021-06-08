27af0de669833286db5c569e942be59df736b9d7
Kamalesh S <73590020+kamalss11@users.noreply.github.com>
2021-06-08 08:57:14 +0530

    Add files via upload

--- JS/js2.js
+++ JS/js2.js
@@ -144,58 +144,58 @@ const db2 = firestore.collection("Trainers")
 const form = document.getElementById("form")
 var storageref
 
-// sbtn.addEventListener("click",function(e){
-//     e.preventDefault()
-//     for(let i=0;i<fields1.length;i++){   
-//         blur(0,n=i)
-//     }
+sbtn.addEventListener("click",function(e){
+    e.preventDefault()
+    for(let i=0;i<fields1.length;i++){   
+        blur(0,n=i)
+    }
     
-//     let nameInput = nam.value
-//     let phoneInput = phone.value
-//     let priceInput = price.value
-//     let rateInput = rate.value
-//     let serviceInput = service.value
-//     let statusInput = sts.value
-//     let addInput = add.value
-//     var image = document.getElementById("img").files[0]
-//     var imgname = image.name
-//     storageref =  firebase.storage().ref()
+    let nameInput = nam.value
+    let phoneInput = phone.value
+    let priceInput = price.value
+    let rateInput = rate.value
+    let serviceInput = service.value
+    let statusInput = sts.value
+    let addInput = add.value
+    var image = document.getElementById("img").files[0]
+    var imgname = image.name
+    storageref =  firebase.storage().ref()
         
-//     const metadata = {
-//         contentType:image.type
-//     }
+    const metadata = {
+        contentType:image.type
+    }
 
-//     var urls 
+    var urls 
     
-//     var uploadImg = storageref.child("images").child(imgname)
-//     uploadImg.put(image,metadata)
-//     .then(snapshot =>{
-//         return uploadImg.getDownloadURL()
-//         .then(url => {
-//             urls = url
-//             console.log(urls)
-//             db.add({
-//                 Name: nameInput,
-//                 Phone: phoneInput,
-//                 Price: priceInput,
-//                 Rate: rateInput,
-//                 Service: serviceInput,
-//                 Status: statusInput,
-//                 Address: addInput,
-//                 DisplayPicture: urls
-//             }).then((docRef)=>{
-//                 console.log("Data Saved.This is you id = > ",docRef.id)
-//                 console.log(nameInput,phoneInput,priceInput,rateInput,serviceInput,statusInput,addInput,imgname)
-//                 form.reset()
-//             })
-//             .catch(function(error){
-//                 console.log(error)
-//             })
-//         })
-//     }).catch(function(error){
-//         console.log(error)
-//     })
-// })
+    var uploadImg = storageref.child("images").child(imgname)
+    uploadImg.put(image,metadata)
+    .then(snapshot =>{
+        return uploadImg.getDownloadURL()
+        .then(url => {
+            urls = url
+            console.log(urls)
+            db.add({
+                Name: nameInput,
+                Phone: phoneInput,
+                Price: priceInput,
+                Rate: rateInput,
+                Service: serviceInput,
+                Status: statusInput,
+                Address: addInput,
+                DisplayPicture: urls
+            }).then((docRef)=>{
+                console.log("Data Saved.This is you id = > ",docRef.id)
+                console.log(nameInput,phoneInput,priceInput,rateInput,serviceInput,statusInput,addInput,imgname)
+                form.reset()
+            })
+            .catch(function(error){
+                console.log(error)
+            })
+        })
+    }).catch(function(error){
+        console.log(error)
+    })
+})
 
 //Forms - form(trainer)
 
@@ -215,7 +215,7 @@ var ste = document.querySelector("#ste")
 var exp = document.querySelector("#exp")
 var tag = document.querySelector("#tag")
 var cat = document.querySelector("#cat")
-var pic = document.querySelector("#pic")
+var pic = document.querySelector("#pic").files[0]
 var inputs2 = document.querySelectorAll(".inp")
 var fields2 = document.querySelectorAll(".field-2")
 var errors2 = document.querySelectorAll(".er")
