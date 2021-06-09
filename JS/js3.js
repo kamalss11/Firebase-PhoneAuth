c956e9b1a5357ef019531463aa40cdba5b1eed2d
Kamalesh S <73590020+kamalss11@users.noreply.github.com>
2021-06-09 17:04:39 +0530

    Add files via upload

--- JS/js3.js
+++ JS/js3.js
@@ -2,6 +2,7 @@ var docId
 var firestore  = firebase.firestore()
 const db = firestore.collection("STUDIOS")
 const db2 = firestore.collection("Trainers")
+
 window.onload = () =>{
     firebase.auth().onAuthStateChanged(function(user) {
         docId = user.uid
@@ -25,12 +26,22 @@ function showPage(){
     document.getElementById("loads").style.display = "none"
 }
 
+var nam = document.querySelector("#name")
+var phone = document.querySelector("#phone")
+var price = document.querySelector("#price")
+var ratings = document.querySelector("#ratings")
+
 db.get().then((querySnapShot)=>{
     querySnapShot.forEach((doc)=>{
         if(docId == doc.id){
             console.log(doc.data())
+            nam.innerHTML = doc.data().Name
+            phone.innerHTML = doc.data().PhoneNumber
+            price.innerHTML = doc.data().Price
+            ratings.innerHTML = doc.data().Ratings
         }
     })
 }).catch(function(error){
     console.log(error)
 })
+
