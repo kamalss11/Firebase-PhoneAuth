0bf37dc97deedeec7ad09f5f86e6a3ada60dd19d
Kamalesh S <73590020+kamalss11@users.noreply.github.com>
2021-06-09 18:06:00 +0530

    Add files via upload

---
+++ JS/editstudio.js
@@ -0,0 +1,183 @@
+// Welcome user 
+
+var usr = document.querySelector("#usr")
+var wel = document.querySelector(".wel")
+var lo = document.querySelector(".outbtn")
+var docId
+
+wel.addEventListener("click",function(){
+    lo.classList.toggle("active")
+})
+
+window.onload = () =>{
+    firebase.auth().onAuthStateChanged(function(user) {
+        docId = user.uid
+        console.log(docId)
+        console.log(user.phoneNumber)
+        usr.innerHTML = user.phoneNumber
+    })
+}
+
+// Signout
+
+function logout(){
+    firebase.auth().signOut()
+    let u = firebase.auth().currentUser
+    console.log(u,"logged out")
+    location.replace("https://phoneauth-dojo.netlify.app/")
+}
+
+// Forms - form(studios)
+
+const sbtn = document.querySelector(".submit")
+
+var nam = document.querySelector("#name")
+var phone = document.querySelector("#phone")
+var price = document.querySelector("#price")
+var rate = document.querySelector("#rate")
+var service = document.querySelector("#service")
+var sts = document.querySelector("#sts")
+var add = document.querySelector("#add")
+var fields1 = document.querySelectorAll(".field-1")
+var inputs = document.querySelectorAll(".inputs")
+const errors = document.querySelectorAll(".error")
+const form = document.getElementById("form")
+        
+var firestore  = firebase.firestore()
+const db = firestore.collection("STUDIOS")
+
+
+// From DB
+
+db.get().then((querySnapShot)=>{
+    querySnapShot.forEach((doc)=>{
+        if(docId == doc.id){
+            console.log(doc.data())
+            nam.value = doc.data().Name
+            phone.value = doc.data().Phone
+            price.value = doc.data().Price
+            rate.value = doc.data().Ratings
+        }
+    })
+}).catch(function(error){
+    console.log(error)
+})
+
+            
+for(let i=0;i<fields1.length;i++){   
+    inputs[i].addEventListener("blur",function(e,n=i){
+        blur(e,n)
+    })
+}
+
+function blur(e,num){
+    let ph = /\d[0-9]{9,}$/
+    let rt = /\d[0-9]{0,}$/
+
+    if(inputs[num].value == ''){
+        errors[num].classList.add("active")
+        ers("This field is required",num)
+    }
+
+    else if(num == 1){
+        if(!inputs[num].value.match(ph)){
+            errors[num].classList.add("active")
+            ers("Enter valid number",num)
+        }
+
+        else{
+            errors[num].classList.remove("active")
+        }
+    }
+
+    else if(num == 3){
+        if(inputs[num].value > 5){
+            errors[num].classList.add("active")
+            ers("Rate out of 5",num)
+        }
+
+        else if(!inputs[num].value.match(rt)){
+            errors[num].classList.add("active")
+            ers("Ratings not match",num)
+        }
+
+        else{
+            errors[num].classList.remove("active")
+        }
+    }
+
+    else{
+        errors[num].classList.remove("active")
+    }
+
+    submitbtn()
+}
+
+function submitbtn(){
+    for(let i=0;i<fields1.length;i++){
+        if(errors[i].classList.contains("active")){
+            sbtn.classList.add("hide")
+            break
+        }
+
+        else{
+            sbtn.classList.remove("hide")
+        }
+    }
+}
+
+function ers(err,nu){
+    errors[nu].innerHTML = err
+}
+
+sbtn.addEventListener("click",function(e){
+    e.preventDefault()
+    for(let i=0;i<fields1.length;i++){   
+        blur(0,n=i)
+    }
+    
+    let nameInput = nam.value
+    let phoneInput = phone.value
+    let priceInput = price.value
+    let rateInput = rate.value
+    let serviceInput = service.value
+    let statusInput = sts.value
+    let addInput = add.value
+    var image = document.getElementById("img").files[0]
+    var imgname = image.name
+    storageref =  firebase.storage().ref()
+        
+    const metadata = {
+        contentType:image.type
+    }
+    
+    storageref = firebase.storage().ref()
+    var urls 
+    var uploadImg = storageref.child("images").child(imgname)
+    uploadImg.put(image,metadata)
+    .then(snapshot =>{
+        return uploadImg.getDownloadURL()
+        .then(url => {
+            urls = url
+            console.log(urls)
+            db.doc(docId).set({
+                Name: nameInput,
+                Phone: phoneInput,
+                Price: priceInput,
+                Ratings: rateInput,
+                Service: serviceInput,
+                Status: statusInput,
+                Address: addInput,
+                DisplayPicture: urls
+            }).then((docRef)=>{
+                console.log("Data Saved.This is you id = > ",docId)
+                console.log(nameInput,phoneInput,priceInput,rateInput,serviceInput,statusInput,addInput,imgname)
+                form.reset()
+            }).catch(function(error){
+                console.log(error)
+            })
+        })
+    }).catch(function(error){
+        console.log(error)
+    })
+})
