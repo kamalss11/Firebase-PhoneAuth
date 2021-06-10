38fc19d1c77b9cbae420cf9a1b5e69e1b3c8abb5
Kamalesh S <73590020+kamalss11@users.noreply.github.com>
2021-06-10 09:27:56 +0530

    Add files via upload

--- JS/logged.js
+++ JS/logged.js
@@ -1,16 +1,16 @@
 // While browser loads
 
 var usr = document.querySelector("#usr")
-var docId
+
 // STUDIOS
 
 window.onload = () =>{
     firebase.auth().onAuthStateChanged(function(user) {
-        docId = user.uid
-        console.log(docId)
+        docId1 = user.uid
+        console.log(docId1)
         if (!user) {
             location.replace("https://phoneauth-dojo.netlify.app/")
-        }
+        } 
 
         else{
             console.log(user.phoneNumber)
@@ -49,16 +49,18 @@ function logout(){
 
 // Form tabs
 
-const formdiv = document.querySelectorAll(".forms")
-const tabs = document.querySelector(".tabs").children
-
-for(let i=0;i<tabs.length;i++){
-    tabs[i].addEventListener("click",function(){
-        for(let j=0;j<tabs.length;j++){
-            tabs[j].classList.remove("active")
-            formdiv[j].classList.remove("active")
+function studio(){
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (!user) {
+            location.replace("https://phoneauth-dojo.netlify.app/studio")
         }
-        formdiv[i].classList.add("active")
-        tabs[i].classList.add("active")
+    })
+}
+
+function trainer(){
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (!user) {
+            location.replace("https://phoneauth-dojo.netlify.app/trainer")
+        }
     })
 }
