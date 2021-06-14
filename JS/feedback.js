3038daa5a5387ebcc6cecaeff36b72d29ca24f86
Kamalesh S <73590020+kamalss11@users.noreply.github.com>
2021-06-14 21:47:25 +0530

    Update feedback.js

--- JS/feedback.js
+++ JS/feedback.js
@@ -19,7 +19,7 @@ dbRef.get().then((snapshot) => {
                let fd = document.createElement("td")
                fd.innerHTML = doc.val().Feedback
                let tm = document.createElement("td")
-               tm.innerHTML = doc.val().Timestamp.seconds() 
+               tm.innerHTML = doc.val().Timestamp.seconds
                tr.append(id,ud,udv,fd,tm)
           })
           i.textContent += docs
